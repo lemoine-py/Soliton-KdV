dc58a97a38acba59d98714ea4d98946d83b3eb72
etienneroger <etienne.roger@student.uclouvain.be>
2024-12-09 23:24:31 +0100

    code upgrade and add graph

--- Python codes/soliton_etienne.py
+++ Python codes/soliton_etienne.py
@@ -1,38 +1,71 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 # Paramètres
 L = 50  # Taille du domaine
 N = 256  # Nombre de points
-dx = L / N
-x = np.linspace(0, L, N, endpoint=False)
+dx = L/N
+x = np.linspace(0, L, N)
 dt = 0.0004  # Pas de temps
 t_max = 140  # Temps maximum
 steps = int(t_max / dt)
 
 # Conditions initiales
-c1, c2 = 0.75, 0.4
-a1, a2 = 0.33, 0.65
-u = (c1/2)*np.cosh((np.sqrt(c1)/2)*(x-a1*L))**(-2) + (c2/2)*np.cosh((np.sqrt(c2)/2)*(x-a2*L))**(-2)
+c1 = 0.75 
+c2 = 0.4
+a1 = 0.33
+a2 = 0.65
 
-# Vecteurs de Fourier
-k = 2 * np.pi * np.fft.fftfreq(N, d=dx)
-k3 = 1j * k**3
+def u_0(x):
+    return (c1/2)*np.cosh((np.sqrt(c1)/2)*(x-a1*L))**(-2) + (c2/2)*np.cosh((np.sqrt(c2)/2)*(x-a2*L))**(-2)
 
-# Résolution avec split-step Fourier
-u_history = [u.copy()]
-for _ in range(steps):
-    # Partie linéaire en espace spectral
-    u_hat = np.fft.fft(u)
-    u_hat = np.exp(k3 * dt) * u_hat
-    u = np.fft.ifft(u_hat).real
+u0 = u_0(x)
+
+
+def solution(u_0):
+    u_history = np.zeros((steps,N))
+    u_history[0] = u_0
+    k = np.fft.fftfreq(N)*N
+    k3 = 1j*(k*2*np.pi/L)**3
+    for i in range(steps):
+        # Partie linéaire
+        u_hat = np.fft.fft(u_0)
+        u_hat = np.exp(k3 * dt) * u_hat
+        u = np.fft.ifft(u_hat).real
+        
+        # Partie non-linéaire
+        u_sq_der = np.fft.ifft(1j*k*np.fft.fft(u**2)*2*np.pi/L)
+        
+        u = u - 3*dt*u_sq_der
+        u = u.real
+        u_0 = u
+        
+        u_history[i] = u
+    return u_history
     
-    # Partie non-linéaire en espace physique
-    u_sq_der = np.fft.ifft(1j*k*np.fft.fft(u**2))
-    
-    u = u - 3*dt*u_sq_der
-    u = u.real
-
-    # Stockage pour visualisation
-    u_history.append(u.copy())
+u_history = solution(u0)
 plt.plot(x,u_history[-1])
+
+
+mask_x = np.linspace(0, N, N, dtype=int, endpoint=False)
+t_plot = np.linspace(0.0, t_max, steps, endpoint=False)
+[xx, tt] = np.meshgrid(x[mask_x], t_plot)
+
+
+fig = plt.figure(figsize=(15, 7))
+gs = gridspec.GridSpec(3, 4, width_ratios=[1.45, 0.1, 0.20, 1.0])
+
+# Contour plot
+ax0 = plt.subplot(gs[:, 0])
+contour = ax0.contourf(
+    xx, tt, u_history, np.linspace(-0.005, 0.4, 100), cmap='Spectral_r')
+ax0.set_title("$N_x = %d, \Delta x = %.4f$" %(256, L/N))
+ax0.set_xlabel("$x$")
+ax0.set_ylabel("$t$")
+# Colorbar
+cax = plt.subplot(gs[:, 1])
+cbar = fig.colorbar(contour, cax=cax)
+cbar.set_ticks(np.linspace(0, 0.3, 6))
+plt.show()
