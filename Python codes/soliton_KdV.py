72c82f7b1acfeddf72fff708b18302ed2d110933
Duck-coder <slshenron@duck.com>
2024-12-10 00:29:08 +0100

    updated README

--- Python codes/soliton_KdV.py
+++ Python codes/soliton_KdV.py
@@ -2,49 +2,60 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import scipy as sp
 from tqdm import tqdm
 
 # Parametres
 L = 50  # Length of the domain
 N = 256  # Number of points
-dx = L / N
-x = np.linspace(0, L, N, endpoint=False)
+dx = L/N
+x = np.linspace(0, L, N)
 
 dt = 0.0004  # Time step
 t_max = 140
 steps = int(t_max / dt)
 
 # Initial conditions
-c1, c2 = 0.75, 0.4
-a1, a2 = 0.33, 0.65
-u = (c1/2)*np.cosh((np.sqrt(c1)/2)*(x-a1*L))**(-2) + (c2/2)*np.cosh((np.sqrt(c2)/2)*(x-a2*L))**(-2)
+c1 = 0.75 
+c2 = 0.4
+a1 = 0.33
+a2 = 0.65
 
-# Fourier vectors
-k = 2 * np.pi * np.fft.fftfreq(N, d=dx)
-k3 = 1j * k**3
+def u_0(x):
+    """ Returns the initial function. """
+    return (c1/2)*np.cosh((np.sqrt(c1)/2)*(x-a1*L))**(-2) + (c2/2)*np.cosh((np.sqrt(c2)/2)*(x-a2*L))**(-2)
 
-print()
-# Resolution with split-step Fourier
-u_history = [u.copy()]
-with tqdm(total=steps) as pbar:
-    for _ in range(steps):
-        # linear part in spectral space
-        u_hat = np.fft.fft(u)
-        u_hat = np.exp(k3 * dt) * u_hat
-        u = np.fft.ifft(u_hat).real
-        
-        # non-linear part in physical space
-        u_sq_der = np.fft.ifft(1j*k*np.fft.fft(u**2))
-    
-        u = u - 3*dt*u_sq_der
-        u = u.real
-    
-        # storage for visualization
-        u_history.append(u.copy())
-        pbar.update(1)
-print()
+u0 = u_0(x)
 
+def solution(u_0):
+    u_history = np.zeros((steps,N))
+    u_history[0] = u_0
+    k = np.fft.fftfreq(N)*N
+    k3 = 1j*(k*2*np.pi/L)**3
+    print()
+    with tqdm(total=steps) as pbar:
+        for i in range(steps):
+            # Partie linéaire
+            u_hat = np.fft.fft(u_0)
+            u_hat = np.exp(k3 * dt) * u_hat
+            u = np.fft.ifft(u_hat).real
+            
+            # Partie non-linéaire
+            u_sq_der = np.fft.ifft(1j*k*np.fft.fft(u**2)*2*np.pi/L)
+            
+            u = u - 3*dt*u_sq_der
+            u = u.real
+            u_0 = u
+            
+            u_history[i] = u
+            pbar.update(1)
+    print()
+    return u_history
+ 
+u_history = solution(u0)
+
+### Simple plot for 4 cases ###
 fig, ax = plt.subplots(1, 4, figsize=(20, 5))
 ax[0].plot(x, u_history[0])
 ax[0].set_title('Initial condition')
@@ -69,4 +80,26 @@ ax[3].set_title('t = {:.2f}'.format(t_max))
 ax[3].set_xlabel('x')
 ax[3].set_ylabel('u')
 ax[3].grid()
+plt.show()
+
+### ----------------------------------------------------------------
+mask_x = np.linspace(0, N, N, dtype=int, endpoint=False)
+t_plot = np.linspace(0.0, t_max, steps, endpoint=False)
+[xx, tt] = np.meshgrid(x[mask_x], t_plot)
+
+fig = plt.figure()
+gs = gridspec.GridSpec(3, 4, width_ratios=[1.45, 0.1, 0.20, 1.0])
+
+# Contour plot
+ax0 = plt.subplot(gs[:, 0])
+contour = ax0.contourf(
+    xx, tt, u_history, np.linspace(-0.005, 0.4, 100), cmap='Spectral_r')
+ax0.set_title("$N_x = %d, \Delta x = %.4f$" %(256, L/N))
+ax0.set_xlabel("$x$")
+ax0.set_ylabel("$t$")
+
+# Colorbar
+cax = plt.subplot(gs[:, 1])
+cbar = fig.colorbar(contour, cax=cax)
+cbar.set_ticks(np.linspace(0, 0.3, 6))
 plt.show()
